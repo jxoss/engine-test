9db277427d97ea75496c57201e638a6622b826f4
Adrian Ottiker <adrian@ottiker.com>
2015-07-29 09:47:47 +0000

    removed obsolete code

--- public/test.js
+++ public/test.js
@@ -31,7 +31,7 @@ exports.init = function () {
     // - ":" -> Data handler
     // - "|" -> Stream handler
     // - ">" -> Broken stream handler
-    this.flows = combinator([":", "|", ">"], this._config.handlerCount || 1);
+    this.flows = combinator([":", "!", "|", ">"], this._config.handlerCount || 1);
 
     // create test flow config for every combination
     for (var i = 0, type, handler, flow; i < this.flows.length; ++i) {
@@ -49,7 +49,10 @@ exports.init = function () {
                     handler = 'stream';
                     break;
                 case '>':
-                    handler = 'broken';
+                    handler = '>stream';
+                    break;
+                case '!':
+                    handler = '!error';
                     break;
 
                 default:
@@ -73,70 +76,6 @@ exports.init = function () {
     }
 };
 
-function streamHandler (A) {
-  
-  // IMPORTANT:
-  // - "FL" stands for this.flow([] || "event") stream.
-  // - "i" is input streams
-  // - "o" are output streams
-  
-  // Stream A setup:
-  // (A.w).o ==> (?.d)
-  //      .i --> (FL.d).o ==> (?.d)
-  //                   .i --> (?.d)
-  // (A.d).o ==> (?.d)
-  
-  // Stream B setup:
-  // (B.w).o ==> (?.d)
-  //      .i --> (?.d)
-  // (B.d).o ==> (?.d)
-  var B = this.flow([] || "event");
-  
-  // Stream C setup (_ext):
-  // (C.w).o ==> (?.d)
-  // (C.d)
-  var C = this.flow([] || "event");
-  C._ext = true;
-  
-  // TODO connect streams with emit and link
-  // TODO show how to connect streams in stream handler
-  
-  // 1. ?.d <--o FL.d <--i (A.w) o--> ?.d
-  A.write(err, data);
-  
-  // 1. ?.d <--i (FL.w) o--> A.d o--> ?.d
-  // 2. ?.d <--o FL.d <--i (A.w) o--> ?.d
-  A.data(A.write.bind(A));
-  
-  // 1. ?.d <--i (FL.w) o--> A.d o--> ?.d
-  // 2. ?.d <--i (B.w) o--> ?.d (note, that writing on B don't emit data on B itself, FL or A.)
-  A.data(stream_B.write.bind(stream_B));
-  
-  // Not connected streams
-  // ------------------------------------------------------------------------------
-  // 2. ?.d <--i (B.w) o--> ?.d
-  B.write(err, data);
-  
-  // 1. ?.d <--i (?.w) o--> B.d o--> ?.d
-  // 2. ?.d <--i (B.w) o--> ?.d
-  B.data(B.write.bind(B));
-  
-  // 1. ?.d <--i (?.w) o--> B.d o--> ?.d
-  // 2. ?.d <--o FL.d <--i (A.w) o--> ?.d
-  B.data(A.write.bind(A));
-  
-  // 1. |i (C.w) o--> ?.d (note, that "_ext" streams only write to the output and NOT on the input)
-  C.write();
-  
-  // 1. <--i (?.w) o--> C.d| (note, that "_ext" streams, WON'T emit the received data further to the ouput)
-  // 2. |i (C.w) o--> ?.d
-  C.data(C.write.bind(C));
-  
-  // 1. <--i (?.w) o--> C.d|
-  // 2. ?.d <--o FL.d <--i (A.w) o--> ?.d
-  C.data(A.write.bind(A));
-}
-
 
 exports.test = function (stream) {
     stream.data([this, function () {
@@ -147,33 +86,35 @@ exports.test = function (stream) {
 };
 
 function callFlows (flows) {
-    for (var i = 0, l = flows.length; i < l; ++i) {
+    for (var i = 0, l = flows.length, stream; i < l; ++i) {
       
-          // emit event
-          this.flow(flows[i][0]).write(
-
-              // error
-              null,
-
-              // write the event name to stream
-              {
-                  id: i,
-                  name: flows[i][0]
-              }
-          );
+          // create stream
+          stream = this.flow(flows[i][0]);
+          
+          // write to data handlers
+          stream.write(null,{
+              id: i,
+              name: flows[i][0]
+          });
+          
+          // write to error handlers
+          stream.write({
+              id: i,
+              name: flows[i][0]
+          });
     }
 }
 
-exports.data = function (err, data, stream, args) {
+exports.data = function (data, stream, args) {
     
     // checks
     // - if expected arguments are received
     if (!args) {
-        console.log('');
+        console.log('Data: No args');
     }
     // - if data object contains all expected data
     if (!data) {
-        console.log('');
+        console.log('Data: No data');
     }
     // - function scope
     // - stream scope
@@ -189,7 +130,35 @@ exports.data = function (err, data, stream, args) {
         type: ':',
         id: args.id,
         name: args.name
-    }, 'successful');
+    }, 'data successful');
+};
+
+exports.error = function (data, stream, args) {
+    
+    // checks
+    // - if expected arguments are received
+    if (!args) {
+        console.log('Error: No args');
+    }
+    // - if data object contains all expected data
+    if (!data) {
+        console.log('Error: No data');
+    }
+    // - function scope
+    // - stream scope
+    // - how many times the handler is called
+    // - if method is called, timeout after a while
+    // - call order
+    
+    //setTimeout(function() {
+    //    stream.write(err, data);
+    //}, 0);
+    
+    this.log('I', {
+        type: ':',
+        id: args.id,
+        name: args.name
+    }, 'error successful');
 };
 
 exports.stream = function (stream, args) {
@@ -217,10 +186,10 @@ exports.broken = function (stream, args) {
     // - if method is called, timeout after a while
     // - call order
 
-    stream._ext = true;
-    stream.data([this, function (err, data, stream, args) {
+    stream._break = true;
+    stream.data([this, function (data, stream, args) {
         setTimeout(function() {
-            stream.write(err, data);
+            stream.write(data);
         }, 0);
     }], args);
 
