aee03c689eb9e554acd0925047bc6457c737761d
Adrian Ottiker <adrian@ottiker.com>
2015-09-07 11:22:11 +0000

    render list with time dealyed stream

--- public/api.js
+++ public/api.js
@@ -1,9 +1,42 @@
 exports.getTests = function (data, stream) {
 
     // implement caching, cause the method can be called directly from a route
-    stream.write(null, {index: 1, name: 'A', duration: 0.003, status: 'Passed', date: '1/1/11'});
-    stream.write(null, {index: 2, name: 'B', duration: 0.003, status: 'Failed', date: '2/2/12'});
-    stream.write(null, {index: 3, name: 'C', duration: 0.003, status: 'Passed', date: '3/3/13'});
+    var send = [
+        {index: 1, name: 'A', duration: 0.003, status: 'Passed', vis_state: 'success', date: '1/1/11'},
+        {index: 2, name: 'B', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 3, name: 'C', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 4, name: 'D', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 5, name: 'E', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 6, name: 'F', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 7, name: 'G', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 8, name: 'H', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 9, name: 'I', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 10, name: 'J', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 11, name: 'K', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 12, name: 'L', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 13, name: 'N', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 14, name: 'M', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 15, name: 'O', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 16, name: 'P', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 17, name: 'Q', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 18, name: 'R', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 19, name: 'S', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 20, name: 'T', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 21, name: 'U', duration: 0.003, status: 'Failed', vis_state: 'danger', date: '2/2/12'},
+        {index: 22, name: 'V', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 23, name: 'W', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 24, name: 'X', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 25, name: 'Y', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'},
+        {index: 26, name: 'Z', duration: 0.003, status: 'Passed', vis_state: 'success', date: '3/3/13'}
+    ];
+
+    var index = 0;
+    var ivid = setInterval(function() {
+        stream.write(null, send[index]);
+        if (++index === send.length) {
+            return clearInterval(ivid);
+        }
+    }, 100);
 };
 
 exports.getTest = function (data, stream) {
