cddffdbbcd2222276bc88ce8ccb2de6a72e58c9b
Adrian Ottiker <adrian@ottiker.com>
2015-09-07 09:15:33 +0000

    working on test app

--- public/api.js
+++ public/api.js
@@ -1,23 +1,32 @@
 exports.getTests = function (data, stream) {
 
     // implement caching, cause the method can be called directly from a route
-    stream.write(null, {name: 'A', duration: 0.003, status: 1, date: '11/11/11'});
-    stream.write(null, {name: 'B', duration: 0.003, status: 1, date: '11/11/11'});
-    stream.write(null, {name: 'C', duration: 0.003, status: 1, date: '11/11/11'});
+    stream.write(null, {index: 1, name: 'A', duration: 0.003, status: 'Passed', date: '1/1/11'});
+    stream.write(null, {index: 2, name: 'B', duration: 0.003, status: 'Failed', date: '2/2/12'});
+    stream.write(null, {index: 3, name: 'C', duration: 0.003, status: 'Passed', date: '3/3/13'});
 };
 
-exports.getTest = function (stream, data) {
+exports.getTest = function (data, stream) {
+
+    stream.write(null, {
+        name: data.name,
+        flow: [
+            [
+                "event1",
+                "method1"
+            ]
+        ]
+    });
+};
+
+exports.saveTest = function (data, stream) {
 
 };
 
-exports.saveTest = function (stream, data) {
+exports.runTests = function (data, stream) {
 
 };
 
-exports.runTests = function (stream, data) {
-
-};
-
-exports.runTest = function (stream, data) {
+exports.runTest = function (data, stream) {
 
 };
