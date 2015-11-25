4dc355a6f72176f970e951bd3df6c4147775e694
Adrian Ottiker <adrian@ottiker.com>
2015-11-25 22:46:11 +0000

    redundant file

--- public/client_api.js
+++
@@ -1,9 +0,0 @@
-exports.data= function (options, data, next) {
-    console.log('DH CALL:', options.id);
-    next(null, data); 
-};
-
-exports.stream = function (stream, options) {
-    // TEST custom stream handlers
-    return stream;
-};
