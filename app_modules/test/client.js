2d32eb6f708c7c2e06074210844928c43361d04d
Adrian Ottiker <adrian@ottiker.com>
2015-11-28 16:17:40 +0000

    clean up test custom modules

--- app_modules/test/client.js
+++ app_modules/test/client.js
@@ -1,30 +1,12 @@
-// run test
-exports.run = function (stream, options) {
-
-    // call test events
-    // render results
-    var pkg = {
-        verify: function () {
-            // ..
-        }
-    };
-
-    /*var flow = this.flow('test');
-    flow.write(pkg);
-    flow.end('endData');
-    flow.on('end');
-    flow.on('error');
-    flow.on('data');*/
-};
-
 // data handler
 exports.data = function (options, data, next) {
-    console.log('Data handler:', options, data);
+    console.log('Data handler:', options.num);
     next(null, data);
 };
 
 // stream handler
 exports.stream = function (stream, options) {
     console.log('Stream handler:', options);
+    return stream; 
 };
 
